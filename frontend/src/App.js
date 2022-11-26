e14e0b8616d004db724f325a0825317e2ca8cc85
Omer <Omersrrol@gmail.com>
2022-11-26 16:46:49 +0200

    Added Elements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
+import data from "./data";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">Amazona</a>
       </header>
-      <main>list products</main>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <img src={product.image} alt={product.name}></img>
+            <div className="product-info">
+              <a href={`/products/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
