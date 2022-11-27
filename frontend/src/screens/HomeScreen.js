630c6060ad60aff059dabaad3bc1bdaeb18314fe
Omer <Omersrrol@gmail.com>
2022-11-27 16:48:12 +0200

    Rendered Backend to Front

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+//import data from "../data";
+import axios from "axios";
+
 function HomeScreen() {
+  // products = variable, setProducts = function to set variables
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("http://localhost:5000/api/products");
+      setProducts(result.data.products); //the products in the backend
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name}></img>
